5895c05a0623382d4ca592c488c32afd5b13558f
Jordan Kula <55468681+jkula25@users.noreply.github.com>
2020-02-10 23:48:12 -0500

    Update Team IDs and Team Scores

--- RTN_Scraper.py
+++ RTN_Scraper.py
@@ -10,6 +10,7 @@ import platform
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+import csv
 
 # Set up chrome webdriver
 chrome_options = Options()
@@ -23,13 +24,16 @@ elif platform.system() == "Linux":
     chrome_driver += "/chromedriver_lin"
 else:
     chrome_driver += "/chromedriver_win.exe"
-driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
 
-def get_team_ids(url):
-    print("Getting team ids")
+def get_team_ids(url, year):
+    print("Getting " + year + " team ids")
     team_ids = {}
     driver.get(url)
-    WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.ID, "year_filter")))
+    select = Select(driver.find_element_by_id("year_filter"))
+    select.select_by_visible_text(year)
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     rows = soup.find_all(class_='rt-tr-group')
     for row in rows:
@@ -44,31 +48,22 @@ def get_team_ids(url):
 
 def get_rosters(team_ids, base_url, year):
     rosters = {}
-    athlete_ids_teams = {}
     print("Getting " + year + " rosters")
     print("--------------------------------------------")
     for team_name, team_id in team_ids.items():
         print(team_name)
         driver.get(base_url + '/' + year + '/' + team_id)
-        WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         rows = soup.find(class_='rosterBox').find(class_='rt-table').find_all(class_='rt-tr-group')
         roster = {}
-        ids = {}
         for row in rows:
-            name = row.find('a')
-            name_id_strarray = str(name.get('href')).split('/')
-            name_id = name_id_strarray[len(name_id_strarray) - 1]
-           
-
             values = row.find_all(class_='rt-td')
             full_name = values[0].string + ' '  + values[1].string
             roster[full_name] = [values[2].string, values[3].string, values[4].string]    
-            ids[full_name] = name_id
-        athlete_ids_teams[team_name] = ids
         rosters[team_name] = roster
     print("--------------------------------------------")   
-    return rosters, athlete_ids_teams
+    return rosters
 
 def get_all_individual_averages(base_url, year, team_ids, rosters):
     averages_team = {}
@@ -78,18 +73,15 @@ def get_all_individual_averages(base_url, year, team_ids, rosters):
     for team_name, team_id in team_ids.items():
         print(team_name)
         driver.get(base_url + '/' + year + '/' + team_id)
-        WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rosterBox")))
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rosterBox")))
         button = driver.find_element_by_xpath('//button[text()="Average"]')
         button.click()
-        WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[style="min-width: 750px;"]')))
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[style="min-width: 750px;"]')))
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         averages = {}
 
         rows = soup.find(class_='rosterBox').find(class_='rt-table').find_all(class_='rt-tr-group')
-        for row in rows:
-            
-
-            
+        for row in rows:            
             values = row.find_all(class_='rt-td')
             full_name = values[0].string + ' '  + values[1].string
             if full_name in rosters[team_name].keys():
@@ -111,22 +103,28 @@ def get_team_scores(team_ids, base_url, year):
     team_scores = {}
     print("Getting " + year + " team scores")
     print("--------------------------------------------")
+    driver.get(base_url)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "allteamscores")))
+    button = driver.find_element_by_xpath("//button[text()='All Team Scores']")
+    button.click()
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
+    select = Select(driver.find_element_by_id("year_filter"))
+    select.select_by_visible_text(year)
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    rows = soup.find(class_='rt-table').find(class_='rt-tbody').find_all(class_='rt-tr-group')
     for team_name, team_id in team_ids.items():
         print(team_name)
-        driver.get(base_url + '/' + year + '/' + team_id)
-        WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "scheduleBox")))
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        rows = soup.find(class_='scheduleBox').find(class_='rt-tbody').find_all(class_='rt-tr')
         scores = {}
         for row in rows:
             values = row.find_all(class_='rt-td')
-            if str(values[2].string) != "None":
-                scores[values[1].string[5:]] = values[2].string
+            if values[0].string == team_name:
+                scores[values[1].string] = values[8].string
         team_scores[team_name] = scores
     print("--------------------------------------------")  
     return team_scores
         
-def get_all_individual_scores(athlete_base_url, year, team_ids, athlete_ids):
+def get_all_individual_scores(athlete_base_url, year, team_ids, rosters):
     scores_ind_team = {}
     print("Getting " + year + " individual scores")
     print("--------------------------------------------")  
@@ -136,24 +134,24 @@ def get_all_individual_scores(athlete_base_url, year, team_ids, athlete_ids):
         print("-------------------------------------------------------------------------")
         driver.get(athlete_base_url + '/' + year + '/' + team_id)
         time.sleep(1)
-        # WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tr-group")))
+        # WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-table")))
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         scores = {}
-        print(len(athlete_ids[team_name]))
-        for index in range(len(athlete_ids[team_name])):
+        print("ROSTER SIZE: " + str(len(rosters[team_name])))
+        for index in range(len(rosters[team_name])):
             select = Select(driver.find_element_by_id('gymnast_filter'))
             athlete_names = soup.find(id='gymnast_filter').find_all('option')
             print(athlete_names[index].string)
             select.select_by_visible_text(athlete_names[index].string)
-            WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tbody")))
+            WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rt-tbody")))
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             rows = soup.find(class_='rt-tbody').find_all(class_='rt-tr-group')
             athlete_name = athlete_names[index].string
             if len(rows)>0:
                 for row in rows:
                     values = row.find_all(class_='rt-td')
-                    print(values[0].string[5:])
                     print("-----------------------")
+                    print(values[0].string[5:])
                     print('FX: ', values[3].string)
                     print('PH: ', values[4].string)
                     print('SR: ', values[5].string)
@@ -178,21 +176,49 @@ def get_all_individual_scores(athlete_base_url, year, team_ids, athlete_ids):
 
             
 
-
-
-def main():
+if __name__ == "__main__":
    # womens_teams_url = 'https://roadtonationals.com/results/charts/'
    # womens_team_base_url = 'https://roadtonationals.com/results/teams/dashboard/2020/'
 
-    mens_teams_url = 'https://roadtonationals.com/results/chartsM/'
+    mens_teams_url = 'https://roadtonationals.com/results/chartsM/allteams'
     mens_athletes_url = 'https://roadtonationals.com/results/teamsM/gymnast'
     mens_team_base_url = 'https://roadtonationals.com/results/teamsM/dashboard'
  
-    mens_team_ids = get_team_ids(mens_teams_url)
-    mens_rosters, athlete_ids = get_rosters(mens_team_ids, mens_team_base_url, "2020")
-    #mens_averages = get_all_individual_averages(mens_team_base_url, "2019", mens_team_ids, mens_rosters)
-    # mens_team_scores = get_team_scores(mens_team_ids, mens_team_base_url, "2020")
-    mens_ind_scores = get_all_individual_scores(mens_athletes_url, "2020", mens_team_ids, athlete_ids)
+    mens_team_ids = get_team_ids(mens_teams_url, "2020")
+    mens_rosters = get_rosters(mens_team_ids, mens_team_base_url, "2020")
+    mens_averages = get_all_individual_averages(mens_team_base_url, "2020", mens_team_ids, mens_rosters)
+    mens_team_scores = get_team_scores(mens_team_ids, mens_teams_url, "2020")
+    mens_ind_scores = get_all_individual_scores(mens_athletes_url, "2020", mens_team_ids, mens_rosters)
+
+
+    # write csv files for all of the mens team scores
+    # year.csv
+    # team name for field names
+    # meet dates
+    # meet scores
+    # path = 'C:/Users/jbkul/Desktop/NQA_System_Research/team_scores_data/'
+    # rtn_years = ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
+    # for year in rtn_years:
+    #     mens_team_ids = get_team_ids(mens_teams_url, year)
+    #     mens_team_scores = get_team_scores(mens_team_ids, mens_teams_url, year)
+    #     filename = year + '_M_scores.csv'
+    #     with open(path + filename, 'w', newline='') as f:
+    #         writer = csv.writer(f)
+    #         for team in mens_team_scores:
+    #             writer.writerow([team])
+    #             dates = list(mens_team_scores[team].keys())
+    #             score_writer = csv.DictWriter(f, fieldnames=dates)
+    #             score_writer.writeheader()
+    #             score_writer.writerow(mens_team_scores[team])
+        
+
+    # for team in mens_team_scores:
+    #     print("--------------------------------------------")
+    #     print(team)
+    #     dates = list(mens_team_scores[team].keys())
+    #     for date in mens_team_scores[team].keys():
+    #         print(date)
+    #         print(mens_team_scores[team][date])
 
     # print("2020 Rosters")
     # for team_name, roster in mens_rosters.items():
@@ -238,4 +264,3 @@ def main():
     #     print()
     # print('---------------------------------------------------------------------')
 
-main()
